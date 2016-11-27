5579ab00da7ed99dd02aa5e481d1602e7e839de5
wisperino <kai_chin@yahoo.com>
2016-11-26 23:37:56 -0500

    Add files via upload

--- assignment4.js
+++ assignment4.js
@@ -1,11 +1,7 @@
-// The anonymous function below will fire on page load
-
 (function() {
-  // Magic!
   console.log('Keepin\'n it clean with an external script!');
-
-  var $url    = $('.flexsearch-input') || '';
-
+  //Something was wrong with the code for the connection so I had to hardcode it.
+  var $url    = 'http://www.mattbowytz.com/simple_api.json?data=all';
   $('#mainForm').on('submit', function(e) {
     e.preventDefault();
     $.ajax({
@@ -13,7 +9,36 @@
       method: 'GET'
     }).success(function(data) {
       console.log(data);
-    }).fail(function(data) {
+	  var interestsArray = data.data.interests;
+	  var programsArray = data.data.programming;
+	  //http://stackoverflow.com/questions/31514909/jquery-ajax-how-to-loop-through-array-as-part-of-ajax-success-function
+	  //var interestArray = JSON.stringify(data.data.interests)
+	  var finalString = ' ';
+	  //loop through the array:
+	  $.each(interestsArray, function( i ) 
+	    {
+		//console.log(val);
+		//I am too tired to figure out how to change the data to a string form so I can have the value in the function loop so just input the value straight up
+		finalString =  finalString + ('<li>'+interestsArray[i]+'</li>');
+		//console.log(finalString);
+		});
+		
+      document.getElementById('interestList').innerHTML = finalString;
+	  finalString = ' ';
+	  
+	  $.each(programsArray, function( i ) 
+	    {
+		//console.log(val);
+		//I am too tired to figure out how to change the data to a string form so I can have the value in the function loop so just input the value straight up
+		finalString =  finalString + ('<li>'+programsArray[i]+'</li>');
+		//console.log(finalString);
+		});
+	  document.getElementById('programingList').innerHTML = finalString;
+	  
+	  
+	  
+    }).fail(function(data) 
+	{
       console.log(data);
     });
   });
